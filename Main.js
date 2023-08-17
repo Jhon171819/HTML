299870932777409a2f8221cdb70cb0be8e95f265
Jhon171819 <jonathancosta1717@gmail.com>
2023-08-17 15:30:51 -0300

    primeiro commit

---
+++ Main.js
@@ -0,0 +1,17 @@
+function Myfunction() {
+
+    let x = document.getElementById("Valor1").value;
+    x = parseInt(x)
+    let y = document.getElementById("Valor2").value;
+    y = parseInt(y)
+
+
+    
+    document.getElementById("Show").innerHTML = x + y
+
+
+
+
+
+
+}
