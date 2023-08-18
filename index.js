c798792a24d40ae85c1f7d38f0e469e2deda077c
Jhon171819 <jonathancosta1717@gmail.com>
2023-08-18 16:37:04 -0300

    quinto commit

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-function WhiteMode() {
+function modoClaro() {
     document.getElementById("B").style.backgroundColor = "white";
     document.getElementById("B").style.color = "black";
-    document.getElementById("outputSoma").style.color = "black";
 }
-function DarkMode() {
+function modoEscuro() {
     document.getElementById("B").style.backgroundColor = "black";
     document.getElementById("B").style.color = "white";
-    document.getElementById("outputSoma").style.color = "white";
 }
-
-function soma() {
+function dividir() {
     let x = document.getElementById("Valor1").value;
     x = parseInt(x)
     let y = document.getElementById("Valor2").value;
     y = parseInt(y)
-    document.getElementById("outputSoma").innerHTML = x + y;
+    resultado = parseFloat(x / y).toFixed(3)
+    document.getElementById("Resultado").innerHTML = resultado;
+}
+
+function subtrair() {
+    let x = document.getElementById("Valor1").value;
+    x = parseInt(x)
+    let y = document.getElementById("Valor2").value;
+    y = parseInt(y)
+    document.getElementById("Resultado").innerHTML = x - y;
+
+}
+function multiplicar() {
+    let x = document.getElementById("Valor1").value;
+    x = parseInt(x)
+    let y = document.getElementById("Valor2").value;
+    y = parseInt(y)
+    document.getElementById("Resultado").innerHTML = x * y;
+}
+
+function somar() {
+    let x = document.getElementById("Valor1").value;
+    x = parseInt(x)
+    let y = document.getElementById("Valor2").value;
+    y = parseInt(y)
+    document.getElementById("Resultado").innerHTML = x + y;
 }
